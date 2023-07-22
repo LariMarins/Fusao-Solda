4c61a31d8d182d4653d58c5f012b935ee43e7113
LariMarins <lari.marins9803@gmail.com>
2023-07-22 08:55:18 -0300

    add componente da navbar

--- assets/index.js
+++ assets/index.js
@@ -4,6 +4,7 @@ export * from './components/logo'
 export * from './components/paragrafo'
 export * from './components/quadro'
 export * from './components/botao'
+export * from './components/navbar'
 
 // seções
 export * from './pages/home-page'
