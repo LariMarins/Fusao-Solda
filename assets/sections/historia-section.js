824b232f72a311be1c136b4913bd31f38a2280c7
larimarins <lari.marins9803@gmail.com>
2023-10-06 20:19:37 -0300

    .

--- assets/sections/historia-section.js
+++ assets/sections/historia-section.js
@@ -14,7 +14,14 @@ export class HistoriaSection extends LitElement {
     render() {
         return html`
          <app-titulo>Sobre nós</app-titulo>
-         <app-paragrafo></app-paragrafo>
+         <app-paragrafo>
+         No ano de 2006 nasce um sonho no coração de um trabalhador, que ao longo de sua trajetória profissional adquiriu conhecimento, experiência e vivência dentro da área de atuação.<br><br>
+E a partir desse sonho e visão empreendedora, que é a capacidade de ver oportunidades onde as pessoas não enxergam, no ano de 2007 o Sr. Ivo Lopes diretor e fundador da empresa partiu em busca de seu ideal, que era abrir o seu próprio negócio. <br><br>
+A partir de então, a meta era se consolidar no mercado metalúrgico como uma empresa que entrega serviços de qualidade, suporte completo aos clientes, ouvindo e transformando o que o cliente deseja em realidade, tornando-se uma empresa com diferencial em suas entregas. 
+
+Hoje são 16 anos de muita dedicação e determinação, que permitiu a sustentabilidade da empresa até aqui.
+
+         </app-paragrafo>
         `;
     }
 }
